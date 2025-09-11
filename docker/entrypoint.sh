52fab6889b110b04548e492916a493973b35959c
Antara <antara10@uw.edu>
2025-09-11 12:03:29 -0700

    commit

---
+++ docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Basic entrypoint for ROS / Colcon Docker containers
+
+# Source ROS 2
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+# Source the base workspace, if built
+if [ -f /workspaces/moveit2_ws/install/setup.bash ]
+then
+    source /workspaces/moveit2_ws/install/setup.bash
+fi
+
+# Source the overlay workspace, if built
+if [ -f /workspaces/shared_ws/install/setup.bash ]
+then
+    source /workspaces/shared_ws/install/setup.bash
+    export INSPECTION_WS=/workspaces/shared_ws
+fi
+
+
+exec "$@"
